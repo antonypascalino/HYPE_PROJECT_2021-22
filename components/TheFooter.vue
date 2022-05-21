7e2e177d3445698512afa9e355f847ec0366deb7
davidecarini <davide.carini@mail.polimi.it>
2022-05-21 12:42:32 +0200

    change footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -6,13 +6,76 @@
 <template>
   <!-- Footer -->
   <footer class="page-footer text-center text-lg-start text-muted">
+
+    <!-- Section: Links  -->
+    <section class="container1">
+      <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <!-- Content -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              <i class="fas fa-gem me-3"></i>
+            </h6>
+              <img class="logo" src="~/static/logoCompatto_bianco.png" alt="logo_compatto_rosso">
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+
+            </h6>
+            <p>
+              <a href="/about" class="text-reset">Chi siamo </a>
+            </p>
+            <p>
+              <a href="/contatti" class="text-reset">Contattaci</a>
+            </p>
+            <p>
+              <a href="/bolognainbreve" class="text-reset">Bologna in Breve</a>
+            </p>
+            <p>
+              <a href="https://www.bolognatoday.it/meteo/" class="text-reset">Meteo</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+            </h6>
+            <p>
+              <a href="/arealegale" class="text-reset">Area Legale</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Informativa sulla Privacy</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Cookie e Pubblicità</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </section>
+    <!-- Section: Links  -->
+
     <!-- Section: Social media -->
     <section
-      class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+      class="container2 d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
     >
       <!-- Left -->
       <div class="me-5 d-none d-lg-block">
-        <span>Rimani connesso sui nostri social Network: </span>
+          © 2022 Tutti i diritti sono riservati:
+          <a class="text-reset fw-bold" href="https://insidebo.herokuapp.com/">insideBO</a>
       </div>
       <!-- Left -->
 
@@ -35,89 +98,6 @@
     </section>
     <!-- Section: Social media -->
 
-    <!-- Section: Links  -->
-    <section class="container1">
-      <div class="container text-center text-md-start mt-5">
-        <!-- Grid row -->
-        <div class="row mt-3">
-          <!-- Grid column -->
-          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-            <!-- Content -->
-            <h6 class="text-uppercase fw-bold mb-4">
-              <i class="fas fa-gem me-3"></i>
-            </h6>
-            <p>
-              <img class="logo" src="~/static/logoCompatto_rosso.png" alt="logo_compatto_rosso">
-            </p>
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold mb-4">
-
-            </h6>
-            <p>
-              <a href="/about" class="text-reset">  Chi siamo </a>
-            </p>
-            <p>
-              <a href="/contatti" class="text-reset">Contattaci</a>
-            </p>
-            <p>
-              <a href="/bolognainbreve" class="text-reset">Bologna in Breve</a>
-            </p>
-            <p>
-              <a href="https://www.bolognatoday.it/meteo/" class="text-reset">Meteo</a>
-            </p>
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold mb-4">
-              LINK UTILI
-            </h6>
-            <p>
-              <a href="/arealegale" class="text-reset">Area Legale</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Informativa sulla Privacy</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Cookie e Pubblicità</a>
-            </p>
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold mb-4">
-             &nbsp; &nbsp; CONTATTI
-            </h6>
-            <p><i class="fas fa-home me-3"></i> Via Mecenate 12, Bologna</p>
-            <p>
-              <i class="fas fa-envelope me-3"></i>
-              insidebo@gmail.com
-            </p>
-            <p><i class="fas fa-phone me-3"></i>TEL: +39 3319212318</p>
-            <p><i class="fas fa-print me-3"></i>FAX: +39 011 7234011</p>
-          </div>
-          <!-- Grid column -->
-        </div>
-        <!-- Grid row -->
-      </div>
-    </section>
-    <!-- Section: Links  -->
-
-    <!-- Copyright -->
-    <div class="copyright text-center p-4">
-      © 2022 Tutti i diritti sono riservati:
-      <a class="text-reset fw-bold" href="https://insidebo.herokuapp.com/">insideBO</a>
-    </div>
-    <!-- Copyright -->
   </footer>
   <!-- Footer -->
 </template>
@@ -131,21 +111,29 @@ export default {
 
 <style scoped>
 
-.copyright{
-  background-color: #C13939;
-  color:white
-}
-.page-footer{
-  background-color: white;
-}
 .container1{
-  background-color: white;
-  color:black
+  background-color: #c13939;
+  color: white;
+  text-decoration: none;
+  border-bottom: 2px solid white;
+}
+.container2{
+  background-color: #c13939;
+  color: white;
 }
 .logo{
   margin: auto;
 }
 .page-footer{
   bottom:0;
+}
+
+a{
+  text-decoration: none;
+}
+
+img{
+  float:inherit;
+  width:10vw
 }
 </style>
