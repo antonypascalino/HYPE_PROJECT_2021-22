82d860aea2f3603e82c45f055b1174901b79b19c
riccardopestrin <10617370@polimi.it>
2022-05-24 15:53:34 +0200

    creazione database + carosello

--- pages/puntidiinteresse.vue
+++ pages/puntidiinteresse.vue
@@ -2,49 +2,41 @@
   <div class="page-container">
     <div class ="app-container">
       <Breadcrumb class="row justify-content-center mt-1" :crumbs="crumbs" @selected="selected"/>
-      <div class = "PDI" :carousel="carousel" @selected="selected">
-        <div class = "title" v-for="image in carousel" :key="image.id">
-          <h1>{{ image.title }}</h1>
-          <div class = "image">
-          <img :src="require(`@/static/puntidiinteresse/${image.img}.jpg`)" alt=""/>
-            </div>
-        <button type="button">Click Me!</button>
       </div>
-    </div>
-      </div>
+    <li v-for="(pointOfInterest, pointOfInterestIndex) of pointOfInterestList"
+        class="col-sm-2 m-2"
+        :key="`pointOfInterest-index-${pointOfInterestIndex}`"
+        :id="pointOfInterest.id"
+        :name="pointOfInterest.name"
+        :img="pointOfInterest.img"
+        :description="pointOfInterest.description"
+    />
   </div>
 </template>
 
 <script>
  import Breadcrumb from '~/components/Breadcrumb.vue';
+
 export default {
   layout: "empty",
   components: {Breadcrumb},
   data() {
     return {
       crumbs: ['HOME', 'PUNTI DI INTERESSE'],
-      carousel: [
-        {id: 1, title: "Piazza Maggiore", img: "1"},
-        {id: 2, title: "Nettuno", img: "2"}
-      ]
     };
-  }
-};
+  },
+    async asyncData({ $axios }) {
+      // const { data } = await $axios.get('http://localhost:3000/api/cats')
+      const { data } = await $axios.get('/api/pointsOfInterest')
+      return {
+        pointOfInterestList: data,
+      }
 
+    }
+};
 
 </script>
 
 <style scoped>
-
-.title{
-  font-family: "Josefin Sans";
-}
-
-.image{
-  size: A4;
-}
-
-
-
 
 </style>
