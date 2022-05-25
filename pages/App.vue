ff7662c3a9538bf1d97f2b40d00774144caa6c58
antonypascalino <antony.pascalino@mail.polimi.it>
2022-05-25 15:53:09 +0200

    Created a carousel (images do not show)

---
+++ pages/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="App">
+    <carousel
+      @next="next"
+      @prev="prev"
+    >
+      <carousel-slide v-for = "(slide,index) in slides"
+                      :key="slide"
+                      :index="index"
+                      :visibleSlide = "visibleSlide"
+                      >
+        <img :src= "slide" :alt="slide"/>
+      </carousel-slide>
+    </carousel>
+  </div>
+</template>
+
+<script>
+import Carousel from "~/components/Carousel";
+import CarouselSlide from "~/components/CarouselSlide";
+
+export default {
+  data() {
+    return {
+        slides: [
+          '@/static/carousel/piazzamaggiore01.jpg',
+          '@/static/carousel/2.jpg',
+          '@/static/carousel/piazzamaggiore02.jpg',
+        ],
+        visibleSlide :0,
+    }
+  },
+  computed: {
+    slidesLen() {
+      return this.slides.length;
+    }
+  },
+  methods : {
+    next() {
+      if(this.visibleSlide >= this.slidesLen - 1 ) {
+        this.visibleSlide = 0;
+      } else {
+        this.visibleSlide++;
+      }
+    },
+    prev() {
+      if(this.visibleSlide <= 0 ) {
+        this.visibleSlide =  this.slidesLen - 1;
+      } else {
+        this.visibleSlide--;
+      }
+    }
+  },
+  components : {
+    Carousel,
+    CarouselSlide,
+  }
+}
+</script>
+
+<style>
+  .app {
+    display: flex;
+    justify-content: center;
+  }
+</style>
