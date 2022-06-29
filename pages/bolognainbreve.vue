65951b2b1a132b5a579f74729bd936f62ccdb7f6
davidecarini <davide.carini@mail.polimi.it>
2022-06-29 15:28:26 +0200

    comments on bolognainbreve.vue

--- pages/bolognainbreve.vue
+++ pages/bolognainbreve.vue
@@ -5,131 +5,102 @@
 
 <template>
   <main class="page-container">
-    <div class="body-container">
-      <StaticHalfImage slide="/bolognaInBreve.jpg" title="BOLOGNA IN BREVE" />
-      <div class="breadcrumb-section">
-        <breadcrumb
-          :default-route="[{ title: 'HOME', path: '/' }]"
-          current-page="Bologna in Breve"
-        />
-      </div>
-      <br /><br />
 
-      <section class="section-la-nostra-missione">
+    <!-- Static Half Image Section -->
+    <StaticHalfImage slide="/bolognaInBreve.jpg" title="BOLOGNA IN BREVE" />
+    <!-- BreadCrumb Section -->
+    <div class="breadcrumb-section">
+      <breadcrumb :default-route="[{ title: 'HOME', path: '/' }]" current-page="Bologna in Breve"/>
+    </div>
+    <br><br>
+
+    <!-- Body Section -->
+    <div class="body-container">
+      <!-- Start Section -->
+      <section class="section-common section-start">
         <p class="bologna-paragraph end-paragraph">
-          <span style="font-weight: bold; color: #C13939; font-style: italic">Capoluogo</span> della Città metropolitana e della regione Emilia-Romagna, Bologna è situata tra le montagne dell'Appennino
-          tosco-emiliano ed il cuore della <b>Pianura Padana</b>.
+          <span style="font-weight: bold; color: #C13939; font-style: italic">Capoluogo</span> della Città metropolitana e della regione Emilia-Romagna, Bologna è situata
+          tra le montagne dell'Appennino tosco-emiliano ed il cuore della <b>Pianura Padana</b>.
         </p>
         <p class="bologna-paragraph end-paragraph">
-        Punto d’incontro tra nord e sud, tra est e ovest, da Bologna si raggiungono
-        facilmente la Riviera Adriatica nonché Venezia, Firenze, Milano e
-        Roma.
+        Punto d’incontro tra nord e sud, tra est e ovest, da Bologna si raggiungono facilmente la Riviera Adriatica nonché Venezia, Firenze, Milano e Roma.
         </p>
         <p class="bologna-paragraph end-paragraph">
-          La città, i cui primi insediamenti risalirebbero almeno al I
-        millennio a.C., fu un importante centro urbano dapprima sotto gli
-        Etruschi e i Celti, poi sotto i Romani e, nel Medioevo, come libero
-        comune. Capitale settentrionale dello Stato Pontificio a partire dal
-        Cinquecento, ebbe un ruolo molto importante durante il Risorgimento e,
-        durante la seconda guerra mondiale, fu uno dei fulcri della
-        Resistenza.
+          La città, i cui primi insediamenti risalirebbero almeno al I millennio a.C., fu un importante centro urbano dapprima sotto gli Etruschi e i Celti, poi sotto i Romani e,
+          nel Medioevo, come libero comune. Capitale settentrionale dello Stato Pontificio a partire dal Cinquecento, ebbe un ruolo molto importante durante il Risorgimento e, durante
+          la seconda guerra mondiale, fu uno dei fulcri della Resistenza.
         </p>
       </section>
 
-      <section class="section-la-nostra-missione">
+      <!-- Perchè-Visitarla Section -->
+      <section class="section-common section-perche-visitarla">
         <div class="title-container">PERCHÈ VISITARLA</div>
+        <!-- Bootstrap Container -->
         <div class="container-fluid">
           <div class="row">
+            <!-- First Column -->
             <div class="col-xl-3 col-lg-4 col-md-6">
-              <img
-                src="~/static/bolognaInBreve.jpg"
-                alt="responsive website"
-                class="img-fluid img1"
-              />
+              <img src="~/static/bolognaInBreve.jpg" alt="bologna-image" class="img-fluid img1"/>
             </div>
+            <!-- Second Column -->
             <div class="col-xl-6 col-lg-8 col-md-6">
               <p class="bologna-paragraph">
                 <span style="font-size: 40px; font-style: italic; color: #C13939; opacity: 0.75"> "Città d’arte, cultura e commercio"</span>
               </p>
               <p class="bologna-paragraph">
-                con un'efficiente struttura
-                fieristica e una rinomata tradizione manifatturiera e
-                motoristica, Bologna è nota per i quasi 40 km di portici, i più
-                lunghi del mondo. Chiamata anche la <b>“Dotta”</b>, per via
-                della sua antica Università, e la “Grassa” per la sua
-                gastronomia, la città si fregia del titolo "Città creativa della
-                Musica UNESCO" e vanta un centro storico medievale tra i più
-                estesi e meglio conservati d’Europa, brulicante di locali,
-                osterie, teatri e botteghe.
+                con un'efficiente struttura fieristica e una rinomata tradizione manifatturiera e motoristica, Bologna è nota per i quasi 40 km di portici, i più lunghi del mondo.
+                Chiamata anche la <b>“Dotta”</b>, per via della sua antica Università, e la “Grassa” per la sua gastronomia, la città si fregia del titolo "Città creativa della Musica UNESCO"
+                e vanta un centro storico medievale tra i più estesi e meglio conservati d’Europa, brulicante di locali, osterie, teatri e botteghe.
               </p>
             </div>
           </div>
         </div>
       </section>
 
-      <section class="section-la-nostra-missione">
+      <!-- Storia Section -->
+      <section class="section-common section-storia">
         <div class="title-container">UN PO' DI STORIA</div>
         <div class="container-fluid">
           <div class="row">
             <div class="col-xl-6 col-lg-8 col-md-6">
               <p class="bologna-paragraph">
-                Nel VI secolo a.C. Bologna fu una delle più importanti città
-                etrusche della Padania e fu nota come Felsina. Nel IV secolo fu
-                occupata dai galli Boi e nei secoli seguenti arrivarono i romani
-                che mutarono il nome in Bononia. Sotto i Romani Bologna fu una
-                città fiorente, con ventimila abitanti, imponenti costruzioni ed
-                un vasto teatro. Mantenne il suo prestigio nei secoli imperiali,
-                ma dell'impero seguì il declino ed il suo perimetro si ridusse a
-                poco a poco.
+                Nel VI secolo a.C. Bologna fu una delle più importanti città etrusche della Padania e fu nota come Felsina. Nel IV secolo fu occupata dai galli Boi e nei secoli seguenti
+                arrivarono i romani che mutarono il nome in Bononia. Sotto i Romani Bologna fu una città fiorente, con ventimila abitanti, imponenti costruzioni ed un vasto teatro. Mantenne
+                il suo prestigio nei secoli imperiali, ma dell'impero seguì il declino ed il suo perimetro si ridusse a poco a poco.
               </p>
             </div>
             <div class="col-xl-3 col-lg-4 col-md-6">
-              <img
-                src="~/static/bolognaStoria01.jpg"
-                alt="responsive webite"
-                class="img-fluid img2"
-              />
+              <img src="~/static/bolognaStoria01.jpg" alt="storia-image" class="img-fluid img2"/>
             </div>
           </div>
         </div>
       </section>
 
-      <section class="section-la-nostra-missione">
+      <!-- Tavola Section -->
+      <section class="section-common section-tavola">
         <div class="title-container">SULLA TAVOLA</div>
         <div class="container-fluid">
           <div class="row">
             <div class="col-xl-3 col-lg-4 col-md-6">
-              <img
-                src="~/static/aTavolaBologna.jpg"
-                alt="responsive webite"
-                class="img-fluid img3"
-              />
+              <img src="~/static/aTavolaBologna.jpg" alt="tavola-image" class="img-fluid img3"/>
             </div>
             <div class="col-xl-6 col-lg-8 col-md-6">
               <p class="bologna-paragraph">
-                <span style="font-size: 40px; font-style: italic; color: #C13939; opacity: 0.75"> "Mortadella, ragù, tortellini, lasagne e
-                  tagliatelle"</span>
+                <span style="font-size: 40px; font-style: italic; color: #C13939; opacity: 0.75"> "Mortadella, ragù, tortellini, lasagne e tagliatelle"</span>
               </p>
-              <p class="bologna-paragraph">
-                Bologna è sinonimo di enogastronomia.
-                <b
-                  >Mortadella, "ragù alla Bolognese", tortellini, lasagne,
-                  tagliatelle, bollito, certosino</b
-                >, sono solo alcuni dei prodotti che l’hanno resa famosa nel
-                mondo insieme alle mitiche sfogline, i cui corsi di cucina sono
-                frequentati da visitatori di ogni continente. Altrettanto noti
-                sono i <b>Vini DOC </b> dei Colli Bolognesi, fra i quali il
-                tipico Pignoletto. La fama internazionale della cucina bolognese
-                risale al Medioevo: erano presenti in città potenti famiglie
-                signorili, presso le cui corti servivano i cuochi più celebrati,
-                come Giulio Cesare Tirelli e Bartolomeo Stefani.
+              <p class="bologna-paragraph"> Bologna è sinonimo di enogastronomia.
+                <b>Mortadella, "ragù alla Bolognese", tortellini, lasagne, tagliatelle, bollito, certosino</b>, sono solo alcuni dei prodotti che l’hanno resa famosa nel
+                mondo insieme alle mitiche sfogline, i cui corsi di cucina sono frequentati da visitatori di ogni continente. Altrettanto noti sono i <b>Vini DOC </b> dei Colli Bolognesi,
+                fra i quali il tipico Pignoletto. La fama internazionale della cucina bolognese risale al Medioevo: erano presenti in città potenti famiglie
+                signorili, presso le cui corti servivano i cuochi più celebrati, come Giulio Cesare Tirelli e Bartolomeo Stefani.
               </p>
             </div>
           </div>
         </div>
       </section>
 
-      <section class="section-la-nostra-missione">
+      <!-- Divertimento Section -->
+      <section class="section-common section-divertimento">
         <div class="title-container">PER DIVERTIRSI</div>
         <div class="container-fluid">
           <div class="row">
@@ -138,44 +109,27 @@
                 <span style="font-size: 40px; font-style: italic; color: #C13939; opacity: 0.75"> "La città pulsa di vita di giorno e di notte"</span>
               </p>
               <p class="bologna-paragraph">
-                La città pulsa di vita di giorno e di notte nelle
-                <b
-                  >numerose osterie, teatri, gallerie, librerie, piazze e
-                  negozi</b
-                >. Nelle suggestive stradine adiacenti Piazza Maggiore, dove si
-                snoda tra botteghe di prodotti tipici il vivace e pittoresco
-                mercato del Quadrilatero, i più golosi non possono rinunciare ad
-                una sosta nel più antico e rinomato laboratorio di cioccolato
-                della città e all’aperitivo in un'antica enoteca. La movida di
-                Bologna non delude mai e dura sino all’alba, animata soprattutto
-                dagli studenti universitari che ai libri alternano il
-                divertimento, una routine ben conosciuta da chi studia in città.
+                La città pulsa di vita di giorno e di notte nelle <b>numerose osterie, teatri, gallerie, librerie, piazze e negozi</b>. Nelle suggestive stradine adiacenti Piazza Maggiore, dove si
+                snoda tra botteghe di prodotti tipici il vivace e pittoresco mercato del Quadrilatero, i più golosi non possono rinunciare ad una sosta nel più antico e rinomato laboratorio di
+                cioccolato della città e all’aperitivo in un'antica enoteca. La movida di Bologna non delude mai e dura sino all’alba, animata soprattutto dagli studenti universitari
+                che ai libri alternano il divertimento, una routine ben conosciuta da chi studia in città.
               </p>
             </div>
             <div class="col-xl-3 col-lg-4 col-md-6">
-              <img
-                src="~/static/divertimento01.jpeg"
-                alt="responsive webite"
-                class="img-fluid"
-              />
+              <img src="~/static/divertimento01.jpeg" alt="enjoy-image" class="img-fluid"/>
             </div>
           </div>
         </div>
       </section>
 
-      <section class="section-la-nostra-missione">
+      <!--End Section -->
+      <section class="section-common section-End">
         <div class="title-container">CONSIGLI UTILI</div>
         <p class="bologna-paragraph end-paragraph">
-          Bologna è ancora una città a misura d’uomo e, almeno nella zona del
-          centro storico, le distanze si coprono a piedi in poco tempo. Arrivati
-          in città, è consigliabile lasciare la macchina in uno dei numerosi
-          parcheggi perché il centro è Zona a Traffico Limitato. Il modo
-          migliore per visitare Bologna è sicuramente a <b>piedi o in bici</b>,
-          anche per via delle ZTL e delle aree pedonali. Il centro è abbastanza
-          raccolto, ma le attrazioni da visitare sono tante, e alcune dislocate
-          anche appena fuori dal centro. Per questo, per un primo approccio alla
-          città vi consigliamo di fare prima un tour guidato a piedi, per
-          individuare i punti di riferimento e conoscere meglio la città.
+          Bologna è ancora una città a misura d’uomo e, almeno nella zona del centro storico, le distanze si coprono a piedi in poco tempo. Arrivati
+          in città, è consigliabile lasciare la macchina in uno dei numerosi parcheggi perché il centro è Zona a Traffico Limitato. Il modo migliore per visitare Bologna è sicuramente
+          a <b>piedi o in bici</b>, anche per via delle ZTL e delle aree pedonali. Il centro è abbastanza raccolto, ma le attrazioni da visitare sono tante, e alcune dislocate anche appena
+          fuori dal centro. Per questo, per un primo approccio alla città vi consigliamo di fare prima un tour guidato a piedi, per individuare i punti di riferimento e conoscere meglio la città.
         </p>
       </section>
 
@@ -188,6 +142,7 @@ import Breadcrumb from '~/components/Breadcrumb.vue'
 import StaticHalfImage from '~/components/StaticHalfImage'
 export default {
   layout: 'default',
+
   components: {
     Breadcrumb,
     StaticHalfImage,
@@ -195,12 +150,12 @@ export default {
 
   head() {
     return {
-      title: 'insideBO | Bologna in Breve',
+      title: 'insideBO • Bologna in Breve',
       meta: [
         {
           hid: 'description',
           name: 'description',
-          content: '',
+          content: 'This page describes the history and the culture of Bologna. ',
         },
       ],
     }
@@ -209,20 +164,15 @@ export default {
 
 </script>
 <style scoped>
-
 .page-container {
   margin-top: -40px;
 }
-
 .body-container {
   margin-top: 60px;
 }
-
 .breadcrumb-section {
   margin-top: 10px;
 }
-
-
 .title-container {
   margin-top: 40px;
   width: 100%;
@@ -231,12 +181,11 @@ export default {
   font-weight: 600;
   font-size: 30px;
   line-height: 50px;
-
   color: #c13939;
   background-color: #f2f2f2;
   padding-left: 80px;
 }
-.section-la-nostra-missione {
+.section-common {
   font-family: 'Raleway', sans-serif;
   font-style: normal;
   font-size: 17px;
@@ -244,38 +193,31 @@ export default {
   margin-right: 8px;
   justify-content: center;
   align-content: center;
-
 }
-
 p.bologna-paragraph {
   padding-top: 10px;
   padding-left: 2vw;
   padding-right: 2vw;
   font-size: 20px;
   text-align: justify;
-
 }
-
 p.end-paragraph{
   padding-left: 8vw;
   padding-right: 8vw;
 }
-
-
 .container-fluid {
   margin-top: 10px;
   justify-content: center;
   align-content: center;
 }
-
 .row {
   align-content: center;
   justify-content: center;
 }
-
 .img-fluid {
   width: 1500px;
 }
+/*Mobile visualization*/
 @media screen and (max-width: 500px) {
   .title-container{
     padding-left: 0px;
